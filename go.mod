84a8e4d3b493eeceaac560fdd44b1fd7a24a5681
allentom <qweasdzxczse2@gmail.com>
2022-03-17 20:37:06 +0800

    add thumbnail plugin

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.16
 
 require (
 	github.com/allentom/haruka v0.0.0-20211105095347-07d9bf2b815d
+	github.com/go-resty/resty/v2 v2.7.0
 	github.com/kardianos/service v1.2.0
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/project-xpolaris/youplustoolkit v0.0.0-20220312090952-4b24318687f4
 	github.com/rs/xid v1.3.0
 	github.com/sirupsen/logrus v1.8.1
@@ -14,6 +16,7 @@ require (
 	go.opentelemetry.io/otel/exporters/jaeger v1.1.0
 	go.opentelemetry.io/otel/sdk v1.1.0
 	go.opentelemetry.io/otel/trace v1.1.0
+	golang.org/x/sys v0.0.0-20220317061510-51cd9980dadf // indirect
 	google.golang.org/grpc v1.45.0
 	gorm.io/driver/mysql v1.1.3
 	gorm.io/driver/sqlite v1.2.3
---
+++ thumbnail/plugin.go
@@ -0,0 +1,72 @@
+package thumbnail
+
+import (
+	"fmt"
+	"github.com/allentom/harukap"
+	"github.com/go-resty/resty/v2"
+	"io/ioutil"
+)
+
+var DefaultThumbnailServicePlugin = &ThumbnailServicePlugin{}
+
+type ThumbnailServicePlugin struct {
+	Client *ThumbnailClient
+}
+
+func (p *ThumbnailServicePlugin) OnInit(e *harukap.HarukaAppEngine) error {
+	logger := e.LoggerPlugin.Logger.NewScope("ThumbnailPlugin")
+	logger.Info("Init ThumbnailPlugin")
+	p.Client = NewThumbnailClient(e.ConfigProvider.Manager.GetString("thumbnail_url"))
+	err := p.Client.Check()
+	if err != nil {
+		return err
+	}
+	logger.Info("Init ThumbnailPlugin success")
+	return nil
+}
+
+type ThumbnailClient struct {
+	BaseUrl string
+}
+
+func NewThumbnailClient(baseUrl string) *ThumbnailClient {
+	return &ThumbnailClient{
+		BaseUrl: baseUrl,
+	}
+}
+
+type ThumbnailOption struct {
+	MaxWidth  int    `hsource:"query" hname:"maxWidth"`
+	MaxHeight int    `hsource:"query" hname:"maxHeight"`
+	Mode      string `hsource:"query" hname:"mode"`
+}
+
+func (c *ThumbnailClient) Generate(sourcePath string, output string, option ThumbnailOption) error {
+	req := resty.New().R().
+		SetFile("file", sourcePath)
+	if option.MaxWidth != 0 {
+		req.SetQueryParam("maxWidth", fmt.Sprintf("%d", option.MaxWidth))
+	}
+	if option.MaxHeight != 0 {
+		req.SetQueryParam("maxHeight", fmt.Sprintf("%d", option.MaxHeight))
+	}
+	if option.Mode != "" {
+		req.SetQueryParam("mode", option.Mode)
+	}
+	response, err := req.Post(c.BaseUrl + "/generator")
+
+	thumbnailContent := response.Body()
+	err = ioutil.WriteFile(output, thumbnailContent, 0644)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func (c *ThumbnailClient) Check() error {
+	_, err := resty.New().R().Get(c.BaseUrl + "/info")
+	if err != nil {
+		return err
+	}
+	return nil
+}
